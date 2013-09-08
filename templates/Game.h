a47efed50fa10eb8fa902bdce18d7c20fd85352e
Adam Harter <athb79@mst.edu>
2013-09-08 18:07:47 -0500

    First draft of GameObjects.h

---
+++ templates/Game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+//temp
+class Game
+{
+    public:
+    ;
+}
+
+#endif
---
+++ templates/GameObjects.h
@@ -0,0 +1,50 @@
+#ifndef GAME_OBJECTS_H
+#define GAME_OBJECTS_H
+
+#include "network.h"
+
+class GameObject
+{
+    public:
+    GameObject(){}
+};
+
+##
+## S 1 FOR MODEL IN MODELS -----------------------------------------------------
+% for model in models:
+## ---------------------------------------------------------------- model.parent
+% if model.parent:
+class ${model.name} : public ${model.parent.name}
+% else:
+class ${model.name} : public GameObject
+% endif
+{
+## ----------------------------------------------------------- model Constructor
+    public:
+    ${model.name}(GameSocket connection, Game* parent_game\
+## put all members in the constructor
+% for datum in model.data:
+, ${type_convert(datum.type)} ${datum.name}\
+% endfor
+);
+## S 2 FOR FUNC IN FUNCTIONS ---------------------------------------------- func
+## -------------------------------------------- model.$(func.name}(${func.args})
+##assume that everything is a bool right now because Russley said to
+% for func in model.functions + model.properties:
+    bool ${func.name}(\
+% for args in func.arguments:
+%   if args == func.arguments[0]:
+${type_convert(args.type)} ${args.name}\
+%   else:
+, ${type_convert(args.type)} ${args.name}\
+%   endif
+% endfor
+);
+% endfor
+};
+
+## E 2 FOR FUNC IN FUNCTIONS ---------------------------------------------- func
+##
+% endfor
+
+#endif
